0f866c7bce2cdba8b1cd3186ac13950a18582c76
LakshitaTak <lakshitatak33@gmail.com>
2024-02-26 20:02:05 +0530

    search

--- src/pages/organization/SearchOrganization.js
+++ src/pages/organization/SearchOrganization.js
@@ -1,9 +1,23 @@
 import CircleIcon from "@mui/icons-material/Circle";
 import { InputAdornment, TextField } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import "./Organization.css";
 
 const SearchOrganization = () => {
+  const [searchQuery, setSearchQuery] = useState("")
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value)
+  }
+
+  const filteredOrgData = orgData.filter(
+    (row) =>
+      row.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      row.domain.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      row.city.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      row.contact.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div className="o-searchbar">
       <TextField
@@ -17,6 +31,8 @@ const SearchOrganization = () => {
             </InputAdornment>
           ),
         }}
+        onChange={handleSearchChange}
+        value={searchQuery}
       />
     </div>
   );
